fb30908b6738831472e0783c43b53600c644c3ad
Davide Bianchi <bianchidavide12@gmail.com>
2023-06-10 18:25:00 +0200

    add role to update svc and deployment

--- internal/controller/echoservice_controller.go
+++ internal/controller/echoservice_controller.go
@@ -45,6 +45,8 @@ type EchoServiceReconciler struct {
 //+kubebuilder:rbac:groups=echo-service-operator.davidebianchi.github.io,resources=echoservices,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=echo-service-operator.davidebianchi.github.io,resources=echoservices/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=echo-service-operator.davidebianchi.github.io,resources=echoservices/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
